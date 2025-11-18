873d642e907aa3e082ef4fe7bd1981eea7c736c0
Bianca Rivera <rivera.bianca@ufl.edu>
2025-11-18 11:02:17 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -8,5 +8,21 @@ import math
 def add(a, b): 
     return a+b
 
+def subtract(a,b):
+    return a-b
+def multiply(a,b):
+    return a*b
+def divide(a,b):
+    if a==0:
+        raise ZeroDivisionError("Cannot divide by zero")
+    return a/b
+
+def logarithm(a,b):
+    if a<-0 or a==1 or b<=0:
+        raise ValueError("Invalid arguments for logarithm")
+    return math.log(b,a)
+
+def exponent(a,b):
+    return a**b
 
 
