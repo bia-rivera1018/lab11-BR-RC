af805c9d44533eb5d7bfbc4fb2cb300e15dee7e0
rodrigoo-cardenas <rodrigo.cardenas@ufl.edu>
2025-11-19 16:50:07 -0500

    new calc

--- calculator.py
+++ calculator.py
@@ -16,7 +16,7 @@ def mul(a,b):
 def div(a,b):
     if a == 0:
         raise ZeroDivisionError("Cannot divide by zero.")
-    return  b/a
+    return b / a
 
 def logarithm(a,b):
     if a<=0 or a==1 or b<=0:
